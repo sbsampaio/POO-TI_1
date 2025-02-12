5ea7b6597c6fb03905f09e33888ba9f13c50a691
liviascopel <livia.scopel@edu.ufes.br>
2025-02-12 19:19:14 -0300

    candidate file reader (chaves e valores selecionados)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        CSVFileReader file = new CandidateFileReader("test.csv");
+        CSVFileReader file = new CandidateFileReader("consulta_cand_2024_ES.csv");
     }
 }
