409bca12144114d98f3c8a6cb1e599b4425c17ca
liviascopel <livia.scopel@edu.ufes.br>
2025-02-12 19:19:14 -0300

    candidate file reader (chaves e valores selecionados)

--- src/CandidateFileReader.java
+++ src/CandidateFileReader.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 enum KEYS_NAME {
-    SG_UE,
-    CD_CARGO,
-    NR_CANDIDATO,
-    NM_URNA_CANDIDATO,
-    NR_PARTIDO,
-    SG_PARTIDO,
-    NR_FEDERACAO,
-    DT_NASCIMENTO,
-    CD_SIT_TOT_TURNO,
-    CD_GENERO;
+    SG_UE, // codigo do municipio
+    CD_CARGO, // codigo do cargo
+    NR_CANDIDATO, // numero do candidato
+    NM_URNA_CANDIDATO, // nome do candidato
+    NR_PARTIDO, // numero do partido
+    SG_PARTIDO, // sigla do partido
+    NR_FEDERACAO, // numero da federacao (-1 para partido isolado)
+    DT_NASCIMENTO, // data de nascimento
+    CD_SIT_TOT_TURNO, // situacao do candidato no turno (2 ou 3 - eleito) (-1 - nao processar)
+    CD_GENERO; // genero (2 - masculino, 4 - feminino)
 
     public int idx() {
         return this.ordinal();
@@ -39,7 +39,7 @@ public class CandidateFileReader implements CSVFileReader {
         String[] keys = null;
 
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file, "ISO-8859-1");
 
             if (scanner.hasNext())
                 keys = scanner.nextLine().split(";");
@@ -52,21 +52,40 @@ public class CandidateFileReader implements CSVFileReader {
             e.printStackTrace();
         }
 
-        return keys;
+        // System.out.println("Keys: " + Arrays.toString(keys));
+
+        String[] selectedKeys = {
+                keys[11], keys[13], keys[16], keys[18], keys[25],
+                keys[26], keys[28], keys[36], keys[48], keys[38]
+        };
+
+        // System.out.println("Chaves selecionadas: " + Arrays.toString(selectedKeys));
+
+        return selectedKeys;
     }
 
     private List<String[]> readValuesCSV(File file) {
         List<String[]> values = new ArrayList<>();
 
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file, "ISO-8859-1");
 
             // discards first line of csv
             if (scanner.hasNextLine())
                 scanner.nextLine();
 
-            while (scanner.hasNextLine())
-                values.add(scanner.nextLine().split(";"));
+            while (scanner.hasNextLine()) {
+                String[] row = scanner.nextLine().split(";");
+
+                String[] selectedValues = {
+                        row[11], row[13], row[16], row[18], row[25],
+                        row[26], row[28], row[36], row[48], row[38]
+                };
+
+                values.add(selectedValues);
+                // System.out.println("Values: " + Arrays.toString(selectedValues));
+
+            }
 
             scanner.close();
         } catch (Exception e) {
